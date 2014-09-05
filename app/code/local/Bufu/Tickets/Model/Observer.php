1ae014feb5b33c0bdaad70948e5240928e814edb
Steffen Mücke <mail@quellkunst.de>
2014-09-05 11:26:02 +0200

    Add prices to quote item for BufuTickets

--- app/code/local/Bufu/Tickets/Model/Observer.php
+++ app/code/local/Bufu/Tickets/Model/Observer.php
@@ -30,4 +30,63 @@ class Bufu_Tickets_Model_Observer
 
         return $this;
     }
+
+	public function checkout_cart_product_add_after(Varien_Event_Observer $observer)
+	{
+		/* @var $item Mage_Sales_Model_Quote_Item */
+		$item = $observer->getQuoteItem();
+		if ($item->getParentItem()) {
+			$item = $item->getParentItem();
+		}
+
+		$request = Mage::app()->getRequest();
+		$ticketParams = $request->getParam("bufu_tickets");
+
+		// load event
+		$event = Mage::getModel('bufu_tickets/event')->load($ticketParams['event_id']);
+		if(!$event) {
+			throw new Mage_Core_Exception("Event not found");
+		}
+
+		if ((int) $ticketParams['nr_normal'] > 0) {
+			// add normal tickets
+			$type = Bufu_Tickets_Helper_Data::TICKET_TYPE_NORMAL;
+			$price = $event->getPriceNormal();
+			$item->setQty((int) $ticketParams['nr_normal']);
+		}
+
+		if ((int) $ticketParams['nr_special'] > 0) {
+			// add special tickets
+			$type = Bufu_Tickets_Helper_Data::TICKET_TYPE_SPECIAL;
+			$price = $event->getPriceSpecial();
+			$item->setQty((int) $ticketParams['nr_special']);
+		}
+
+		$item->setCustomPrice($price);
+		$item->setOriginalCustomPrice($price);
+		$item->getProduct()->setIsSuperMode(true);
+
+		$item->addOption(array(
+			'item' => $item,
+			'code' => Bufu_Tickets_Helper_Data::OPTION_TYPE,
+			'value' => $type
+		));
+		$item->addOption(array(
+			'item' => $item,
+			'code' => Bufu_Tickets_Helper_Data::OPTION_EVENT_ID,
+			'value' => $event->getId()
+		));
+		$item->addOption(array(
+			'item' => $item,
+			'code' => Bufu_Tickets_Helper_Data::OPTION_PRICE,
+			'value' => $price
+		));
+
+//		// add bufu_ticket options
+//		$product->addCustomOption(Bufu_Tickets_Helper_Data::OPTION_TYPE, $type);
+//		$product->addCustomOption(Bufu_Tickets_Helper_Data::OPTION_EVENT_ID, $event->getId());
+//		$product->addCustomOption(Bufu_Tickets_Helper_Data::OPTION_PRICE, $price);
+
+		return $this;
+	}
 }
