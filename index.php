7ec11b1629b40756692c0b25b6343f3fac355128
Steffen Mücke <mail@quellkunst.de>
2018-11-02 14:18:31 +0100

    change AoE file privileges, improve index.php

--- index.php
+++ index.php
@@ -24,15 +24,15 @@
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
-if (version_compare(phpversion(), '5.3.0', '<')===true) {
-    echo  '<div style="font:12px/1.35em arial, helvetica, sans-serif;">
-<div style="margin:0 0 25px 0; border-bottom:1px solid #ccc;">
-<h3 style="margin:0; font-size:1.7em; font-weight:normal; text-transform:none; text-align:left; color:#2f2f2f;">
-Whoops, it looks like you have an invalid PHP version.</h3></div><p>Magento supports PHP 5.3.0 or newer.
-<a href="http://www.magentocommerce.com/install" target="">Find out</a> how to install</a>
- Magento using PHP-CGI as a work-around.</p></div>';
-    exit;
-}
+//if (version_compare(phpversion(), '5.3.0', '<')===true) {
+//    echo  '<div style="font:12px/1.35em arial, helvetica, sans-serif;">
+//<div style="margin:0 0 25px 0; border-bottom:1px solid #ccc;">
+//<h3 style="margin:0; font-size:1.7em; font-weight:normal; text-transform:none; text-align:left; color:#2f2f2f;">
+//Whoops, it looks like you have an invalid PHP version.</h3></div><p>Magento supports PHP 5.3.0 or newer.
+//<a href="http://www.magentocommerce.com/install" target="">Find out</a> how to install</a>
+// Magento using PHP-CGI as a work-around.</p></div>';
+//    exit;
+//}
 
 /**
  * Compilation includes configuration file
@@ -68,6 +68,7 @@ require_once $mageFilename;
 
 if (isset($_SERVER['MAGE_IS_DEVELOPER_MODE'])) {
     Mage::setIsDeveloperMode(true);
+    ini_set('display_errors', 1);
 }
 
 #ini_set('display_errors', 1);
